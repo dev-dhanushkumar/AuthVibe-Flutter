007e266be2dd0b044edca813c311686c315c24b2
dev-dhanushkumar <dhanush.m@kriyaetive.com>
2025-06-14 15:27:24 +0530

    Entry point setup

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:quickauth_flutter/signin.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Signin(),
+  ));
+}
